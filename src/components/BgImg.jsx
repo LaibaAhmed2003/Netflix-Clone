84272605a08db3cc8dad4d230fc629b90753a0e7
Laiba <laiba.sabahat9@gmail.com>
2023-12-18 20:14:20 +0500

    signup page created

---
+++ src/components/BgImg.jsx
@@ -0,0 +1,9 @@
+import NetflixBg from "../images/netflix-background.jpg";
+const BgImg = () => {
+  return (
+    <div className="BgImg">
+      <img src={NetflixBg} alt="" />
+    </div>
+  );
+};
+export default BgImg;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,24 @@
+import BgImg from "../components/bgImg";
+
 const Signup = () => {
   return (
     <>
-      <h1>Signup page</h1>
+      <BgImg />
+      <div className="Signup-Container">
+        <div className="Signup-Content">
+          <h1>Unlimited movies, TV shows and more</h1>
+          <h4>Watch anywhere, Cancel anytime</h4>
+          <h6>
+            Ready to watch? enter your email to create or restart membership
+          </h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder="Email Address" name="email" />
+          <input type="password" placeholder="Password" />
+          <button>Get Started</button>
+          <button>Log In</button>
+        </div>
+      </div>
     </>
   );
 };
